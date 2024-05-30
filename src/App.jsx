6da22e68fda7f865575c9582d18cf16cdfe6969e
Kevin <kevindubon43@gmail.com>
2024-05-29 17:52:41 -0700

    Foundation

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,20 @@ import './App.css'
 function App() {
 
   return (
-    <>
-    </>
+    <div className='content'>
+      <nav className='navbar'>
+        <p className='pageTitle'>California Card Games Guide</p>
+      </nav>
+      <main className='contentContainer'>
+        <div className='gameContainer'><div className='gameName'>Ez Baccarat</div></div>
+        <div className='gameContainer'><div className='gameName'>BlackJack</div></div>
+        <div className='gameContainer'><div className='gameName'>Three Card Poker</div></div>
+        <div className='gameContainer'><div className='gameName'>Crazy 4 Poker</div></div>
+        <div className='gameContainer'><div className='gameName'>Ultimate Texas Hold &apos;em</div></div>
+        <div className='gameContainer'><div className='gameName'>Pai Gow Poker</div></div>
+        <div className='gameContainer'><div className='gameName'>Mississippi Stud</div></div>
+      </main>
+    </div>
   )
 }
 
