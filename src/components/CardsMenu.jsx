fac369def2b529f6e60e8f5773c0dfe310a65256
Kevin <kevindubon43@gmail.com>
2024-05-31 15:03:32 -0700

    Add card flip

--- src/components/CardsMenu.jsx
+++ src/components/CardsMenu.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function CalCards() {
-
+  const [clickedIndex, setClickedIndex] = useState(null)
   const calGames = ['Ez Baccarat', 'BlackJack', 'Three Card Poker', 'Crazy 4 Poker', "Ultimate Texas Hold 'em", 'Pai Gow Poker', 'Mississippi Stud']
 
+  const handleClick = (index) => {
+    setClickedIndex(index)
+  }
+  
+  const handleBackClick = (event) => {
+    event.stopPropagation()
+    setClickedIndex(null);
+  }
 
     return (
       <>
@@ -15,7 +24,16 @@ function CalCards() {
         <div className='content'>
         <div className='contentContainer'>
           {calGames.map((item, index) => (
-            <div key={index} className='gameContainer'><div className='gameName'>{item}</div></div>
+            <div key={index} onClick={() => handleClick(index)} className={`gameContainer ${clickedIndex === index ? 'flip' : ''}`}>
+              <div className='gameName'>
+                {item}
+                </div>
+                <div className="back">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, porro maxime? Odio nostrum consequatur magni vel voluptate veniam veritatis tenetur voluptas reiciendis, commodi tempora hic sequi eos quibusdam, pariatur harum!</p>
+                  <button>Learn more</button>
+                  <button onClick={(event) => handleBackClick(event)}>close</button>
+                </div>
+            </div>
           ))}
         </div>
       </div>
